628013513b1c57ae1ea45a0feda8a5228dc9acfd
Jon <jonc7@vt.edu>
2018-03-13 16:35:07 -0400

    Done?!?

--- L15/opi.c
+++ L15/opi.c
@@ -2,28 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char **argv) {
+#include <omp.h>
+
+int main(int argc, char** argv) {
 
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
-  int Nthreads = 1;
-  
+  int Nthreads = atoi(argv[1]);
+  omp_set_num_threads(Nthreads);
+  double time = omp_get_wtime();
+
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
-  long int seed = 0;
-  srand48_r(seed, drandData+0);
+  #pragma omp parallel
+  {
+  int rank = omp_get_thread_num();
+  long int seed = rank;
+  srand48_r(seed, drandData+rank);
+  }
+  long long int Ntrials = 10000000/Nthreads;
 
-  long long int Ntrials = 10000000;
-
-
+  
   //need running tallies
   long long int Ntotal=0;
   long long int Ncircle=0;
 
+  #pragma omp parallel reduction(+:Ncircle,Ntotal)
+  {
+  #pragma omp for
   for (long long int n=0; n<Ntrials; n++) {
     double rand1;
     double rand2;
@@ -39,14 +49,16 @@ int main(int argc, char **argv) {
     if (sqrt(x*x+y*y)<=1) Ncircle++;
     Ntotal++;
 
-    if (n%100 ==0) {
+    /*if (n%100 ==0) {
       double pi = 4.0*Ncircle/ (double) (n);
       printf("Our estimate of pi is %g \n", pi);
-    }
+    }*/
   }
-
+  }
+  
   double pi = 4.0*Ncircle/ (double) (Ntotal);
   printf("Our final estimate of pi is %g \n", pi);
+  printf("Running time: %f\n", omp_get_wtime()-time);
 
   free(drandData);
   
